a5376780e79c0b9b1069b68d5179739761c110b4
alunturner <alun.turner@googlemail.com>
2025-08-15 07:48:13 +0100

    add a clean input function

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	fmt.Printf("Hello, World!")
 }
+
+func cleanInput(text string) []string {
+	words := strings.Fields(text)
+
+	for i, word := range words {
+		words[i] = strings.ToLower(word)
+	}
+
+	return words
+}
+
