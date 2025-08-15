3e783bb695088cf592b04ca1a2c0ef45be5fcdb1
alunturner <alun.turner@googlemail.com>
2025-08-15 08:24:39 +0100

    add scanner to read command

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Printf("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		if !scanner.Scan() {
+			break
+		}
+		line := scanner.Text()
+		cleanedInput := cleanInput(line)
+
+		if len(cleanedInput) == 0 {
+			fmt.Println("You did not enter any input!")
+		} else {
+			fmt.Println("Your command was:", cleanInput(line)[0])
+		}
+	}
 }
 
 func cleanInput(text string) []string {
