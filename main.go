19ff605c71ee1353d57f15e232cd5abc48f5c7e0
alunturner <alun.turner@googlemail.com>
2025-08-17 07:49:28 +0100

    start building the first commands

--- main.go
+++ main.go
@@ -7,7 +7,41 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name string
+	description string
+	callback func() error
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println("")
+	fmt.Println("help: Displays a help message")
+	fmt.Println("exit: Exit the Pokedex")
+	return nil
+}
+
 func main() {
+	cmdMap := map[string]cliCommand {
+		"exit": {
+			name: "exit",
+			description: "Exit the Pokedex",
+			callback: commandExit,
+		},
+		"help": {
+			name: "help",
+			description: "Displays a help message",
+			callback: commandHelp,
+		},
+	}
+
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
@@ -16,12 +50,22 @@ func main() {
 			break
 		}
 		line := scanner.Text()
-		cleanedInput := cleanInput(line)
+		cleanedLine := cleanInput(line)
 
-		if len(cleanedInput) == 0 {
-			fmt.Println("You did not enter any input!")
+		if len(cleanedLine) == 0 {
+			fmt.Println("No command entered")
+			continue
+		}
+
+		cmd := cleanedLine[0]
+
+		if c, ok := cmdMap[cmd]; ok {
+			err := c.callback()
+			if err != nil {
+				fmt.Println("%v", err)
+			}
 		} else {
-			fmt.Println("Your command was:", cleanInput(line)[0])
+			fmt.Println("Unknown command")
 		}
 	}
 }
